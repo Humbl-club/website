2b4da5baddcdf71a466554a87ecc47a84ec2a9c1
Zhenya Glibko <zhenyaglibko@gmail.com>
2024-11-28 17:29:19 +0300

    product complete

---
+++ frontend/islands/product-reviews.js
@@ -0,0 +1,30 @@
+import '@/styles/blocks/product-reviews.scss'
+
+class ProductReviews extends window.HTMLElement {
+  connectedCallback() {
+    const averageRateBlock = this.querySelector(
+      '.jdgm-rev-widg__summary-average'
+    )
+    const reviewsNumberBlock = this.querySelector(
+      '.jdgm-rev-widg__summary-text'
+    )
+
+    if (reviewsNumberBlock) {
+      let text = reviewsNumberBlock.innerHTML.split('Based on')
+      if (text.length > 0) {
+        text = text[1]
+        reviewsNumberBlock.innerHTML = text
+      }
+    }
+
+    if (averageRateBlock) {
+      let averageRate = averageRateBlock.innerHTML.split('out of')
+      if (averageRate.length) {
+        averageRate = averageRate[0]
+        averageRateBlock.innerHTML = Number(averageRate).toFixed(1)
+      }
+    }
+  }
+}
+
+window.customElements.define('product-reviews', ProductReviews)
