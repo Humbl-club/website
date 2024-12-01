f86d081459778abd91f763982690905bf2268e89
hanomag123 <zhenyaglibko@gmail.com>
2024-12-01 11:09:59 +0300

    product-available

--- frontend/islands/product-select.js
+++ frontend/islands/product-select.js
@@ -1,7 +1,47 @@
-class ProductSwiper extends window.HTMLElement {
+class ProductSelect extends window.HTMLElement {
   connectedCallback() {
-    console.log('helo')
+    this.init()
+
+    document.addEventListener('click', this.closeAllSelects)
+  }
+
+  init() {
+    this.dropdown = this.querySelector('.product-dropdown')
+    this.selected = this.querySelector('.product-selected')
+    if (this.selected && this.dropdown) {
+      this.selected.addEventListener('click', (event) => {
+        event.stopPropagation()
+        this.closeAllSelects(this)
+        this.classList.toggle('active')
+      })
+
+      this.mobileText = this.selected.querySelector('.product-select-mobile')
+
+      this.dropdownlist = this.dropdown.querySelectorAll('.product-select-item')
+      if (this.dropdownlist.length) {
+        this.dropdownlist.forEach((el) => {
+          el.addEventListener('click', () => {
+            this.dropdownlist.forEach((item) => {
+              item.classList.remove('same-as-selected')
+            })
+            el.classList.add('same-as-selected')
+            this.mobileText.innerText = el.innerText
+          })
+        })
+      }
+    }
+  }
+
+  closeAllSelects(current) {
+    const allSelects = document.querySelectorAll('product-select')
+    if (allSelects.length) {
+      allSelects.forEach((el) => {
+        if (el !== current) {
+          el.classList.remove('active')
+        }
+      })
+    }
   }
 }
 
-window.customElements.define('product-swiper', ProductSwiper)
+window.customElements.define('product-select', ProductSelect)
