edab13a7f458ae54f3fe44441bd91110aab3a6ae
Zhenya Glibko <zhenyaglibko@gmail.com>
2024-12-11 18:46:23 +0300

    fix

---
+++ frontend/islands/card-favorite.js
@@ -0,0 +1,33 @@
+class CardFavorite extends window.HTMLElement {
+  connectedCallback() {
+    const mainFavoriteBtn = this.querySelector('.card-favorite')
+    const favoriteBtn = this.querySelector('.st-wishlist-button')
+
+    if (favoriteBtn && mainFavoriteBtn) {
+      const observer = new window.MutationObserver((mutationRecords) => {
+        mutationRecords.forEach((record) => {
+          mainFavoriteBtn.classList.toggle(
+            'active',
+            record.target.classList.contains('st-is-added')
+          )
+
+          document.body.classList.toggle(
+            'loading',
+            record.target.classList.contains('st-is-loading')
+          )
+        })
+      })
+
+      observer.observe(favoriteBtn, {
+        attributes: true,
+        attributeFilter: ['class']
+      })
+
+      mainFavoriteBtn.addEventListener('click', function () {
+        favoriteBtn.click()
+      })
+    }
+  }
+}
+
+window.customElements.define('card-favorite', CardFavorite)
