6c52632b2235041d3fe535501c432322522f95a6
Vadim <peskovski.vadim@gmail.ru>
2024-12-13 15:02:42 +0300

    contact form

--- frontend/islands/contact-form.js
+++ frontend/islands/contact-form.js
@@ -1,24 +1,120 @@
 import '@/styles/blocks/contact-form.scss'
-import 'intl-tel-input/build/css/intlTelInput.css';
-import intlTelInput from 'intl-tel-input';
 
 class ContactForm extends window.HTMLElement {
-  connectedCallback() {
-    const form = document.querySelector('.contact');
+    connectedCallback() {
+        this.init()
 
-    if (form) {
-      let phone_input = document.querySelector("#ContactForm-phone");
+        //email validation
+        let emailInput = this.querySelector('#ContactForm-email'),
+            errorElement = this.querySelector("#errorField"),
+            btnSubmit = this.querySelector('button[type="submit"]');
 
-      let iti = intlTelInput(phone_input, {
-        separateDialCode: true,
-        onlyCountries: [],
-        geoIpLookup: false,
-        initialCountry: "Anguilla",
-        loadUtils: () => import("intl-tel-input/utils"),
-        utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.8/build/js/utils.js"
-      });
+        emailInput.addEventListener('input', (event) => {
+            setTimeout(function () {
+                let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    emailMsg = 'Enter a valid email',
+                    emailValue = emailInput.value.trim();
+
+                if (!emailRegex.test(emailValue)) {
+                    emailInput.classList.add("error");
+                    errorElement.innerHTML = emailMsg;
+                    btnSubmit.setAttribute('disabled', 'true');
+
+                } else {
+                    if (emailInput.classList.contains('error')) {
+                        emailInput.classList.remove("error");
+                    }
+
+                    btnSubmit.removeAttribute('disabled');
+                    errorElement.innerHTML = "";
+                }
+            }, 1000)
+        });
+
+
+        //names validation
+        let nameFields = this.querySelectorAll('.names');
+
+        nameFields.forEach(function (field) {
+            field.addEventListener('input', function (event) {
+                let firstNameInput = document.querySelector('#ContactForm-first-name'),
+                    lastNameInput = document.querySelector('#ContactForm-last-name'),
+                    errorElement = document.querySelector("#errorField");
+
+                setTimeout(function () {
+                    function validateField(input) {
+                        let value = input.value.trim();
+                        if (/^\d+$/.test(value)) return false;
+                        return value.length > 0;
+                    }
+
+                    let firstNameValid = validateField(firstNameInput),
+                        lastNameValid = validateField(lastNameInput),
+                        nameMsg = 'The name field must be filled in and must not contain spaces and numbers only';
+
+                    if (!firstNameValid) {
+                        errorElement.innerHTML = nameMsg;
+                        addError(firstNameInput);
+                    } else {
+                        removeError(firstNameInput);
+                    }
+
+                    if (!lastNameValid) {
+                        errorElement.innerHTML = nameMsg;
+                        addError(lastNameInput);
+                    } else {
+                        removeError(lastNameInput);
+                    }
+
+                    function addError(field) {
+                        field.classList.add('error');
+                        btnSubmit.setAttribute('disabled', 'true');
+                    }
+
+                    function removeError(field) {
+                        field.classList.remove('error');
+                        errorElement.innerHTML = '';
+                        btnSubmit.removeAttribute('disabled');
+                    }
+                }, 1000)
+            });
+        })
     }
-  }
+
+//ajax submit
+    init() {
+        this.form = this.querySelector('form')
+        if (this.form) {
+            this.submit = this.form.querySelector('[type="submit"]')
+            this.submit.addEventListener('click', (event) => {
+                event.preventDefault()
+                this.renderSectionsFromFetch(this.form.action, this.form)
+            })
+        }
+    }
+
+    async renderSectionsFromFetch(url, form) {
+        const formData = new FormData(form);
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                body: formData
+            })
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            let html = await response.text(),
+                parser = new window.DOMParser().parseFromString(html, 'text/html'),
+                form = parser.getElementById('ContactForm');
+
+            this.innerHTML = form.innerHTML;
+            this.init();
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
 }
 
 window.customElements.define('contact-form', ContactForm)
