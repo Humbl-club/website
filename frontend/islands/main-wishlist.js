82019a3945ecec90af2164482503e9a3005c3b14
Zhenya Glibko <zhenyaglibko@gmail.com>
2024-12-13 12:28:12 +0300

    complete wishlist

--- frontend/islands/main-wishlist.js
+++ frontend/islands/main-wishlist.js
@@ -4,26 +4,57 @@ class MainWishlist extends window.HTMLElement {
   connectedCallback() {
     console.log('main-wishlist connected')
 
-    const mainFavoriteBtn = this.querySelector('.favorite-label')
     const wishlistPage = document.getElementById('st-wishlist-page')
 
-    if (wishlistPage && mainFavoriteBtn) {
+    if (wishlistPage) {
+      this.appendChild(wishlistPage)
       const observer = new window.MutationObserver((mutationRecords) => {
         mutationRecords.forEach((record) => {
-          mainFavoriteBtn.classList.toggle(
-            'active',
-            record.target.classList.contains('st-is-added')
-          )
+          if (record.target === wishlistPage) {
+            document.body.classList.toggle(
+              'loading',
+              record.target.classList.contains('st-wishlist-loading')
+            )
+          }
 
-          document.body.classList.toggle(
-            'loading',
-            record.target.classList.contains('st-is-loading')
-          )
+          if (record.type === 'childList') {
+            record.addedNodes.forEach((el) => {
+              if (el.classList.contains('st-product-card')) {
+                const form = el.querySelector('form')
+                if (form) {
+                  const removeBtn = document.createElement('button')
+                  const nativeRemove = el.querySelector('.st-delete-button')
+                  const block = document.createElement('div')
+                  const addBtn = document.createElement('button')
+                  addBtn.type = 'submit'
+                  addBtn.classList.add('link')
+                  addBtn.innerHTML = 'Add to bag'
+                  block.classList.add('main-wishlist-bot')
+                  removeBtn.classList.add('link')
+                  removeBtn.innerHTML = 'Remove'
+                  removeBtn.type = 'button'
+
+                  if (nativeRemove) {
+                    removeBtn.addEventListener('click', function () {
+                      nativeRemove.click()
+                    })
+                    block.appendChild(removeBtn)
+                    form.appendChild(block)
+                  }
+
+                  // block.appendChild(addBtn)
+                }
+              }
+            })
+          }
+          console.log(record)
         })
       })
 
       observer.observe(wishlistPage, {
         attributes: true,
+        childList: true,
+        subtree: true,
         attributeFilter: ['class']
       })
     }
