3bfc3d22ffdd2cb3e78f14cbdd2baae032bd2dec
Zhenya Glibko <zhenyaglibko@gmail.com>
2024-12-13 18:02:14 +0300

    just-validate

---
+++ frontend/islands/just-validate.js
@@ -0,0 +1,65 @@
+import JustValidate from 'just-validate'
+import JustValidatePluginDate from 'just-validate-plugin-date'
+
+class ValidateIsland extends window.HTMLElement {
+  constructor() {
+    super()
+
+    this.options = {
+      errorLabelStyle: {
+        color: 'red'
+      },
+      errorLabelCssClass: ['error-text'],
+      errorFieldCssClass: ['error'],
+      validateBeforeSubmitting: true,
+      errorsContainer: '#errors-container_custom-name'
+    }
+
+    this.validate = new JustValidate(this.querySelector('form'), this.options)
+  }
+
+  connectedCallback() {
+    const redEmail = this.querySelector('.email-required')
+    if (redEmail) {
+      this.validate.addField(redEmail, [
+        {
+          rule: 'email'
+        }
+      ])
+    }
+
+    const date = this.querySelector('.date-validation')
+
+    if (date) {
+      this.validate.addField(date, [
+        {
+          plugin: JustValidatePluginDate(() => ({
+            format: 'mm/dd/yyyy'
+          })),
+          errorMessage: 'Date should be in dd/MM/yyyy format'
+        }
+      ])
+    }
+
+    const requiredTexts = this.querySelectorAll('.required-text')
+    if (requiredTexts.length) {
+      requiredTexts.forEach((el) => {
+        this.validate.addField(el, [
+          {
+            rule: 'required'
+          },
+          {
+            rule: 'minLength',
+            value: 3
+          },
+          {
+            rule: 'maxLength',
+            value: 50
+          }
+        ])
+      })
+    }
+  }
+}
+
+window.customElements.define('just-validate', ValidateIsland)
