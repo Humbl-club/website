39a219c2f6d864dbd49a8105022ba363d4a5a278
hanomag123 <zhenyaglibko@gmail.com>
2024-12-15 22:04:21 +0300

    profile start

--- frontend/entrypoints/customers.js
+++ frontend/entrypoints/customers.js
@@ -11,9 +11,17 @@ if (links.length) {
       link.classList.remove('active')
     }
 
+    const isActive = document.querySelector(
+      '.account-aside .account-link.active'
+    )
+
     link.addEventListener('click', function () {
       links.forEach((el) => el.classList.remove('active'))
       this.classList.add('active')
     })
+
+    if (Boolean(isActive) === false) {
+      links[0].classList.add('active')
+    }
   })
 }
